b147e971ee43cf744ba071e539bf320d5bb1ad28
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-18 12:45:26 -0500

    created api and html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,12 @@
+const noteData = require('../db/db.json');
+
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => res.json(noteData));
+
+    app.post('/api/notes', (req, res) => {
+        const newNote = req.body;
+        
+        noteData.push(newNote);
+        res.json(newNote);
+    });
+};
