df9bab52e0c5ac218949f7d785f13b00b768b861
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-18 15:57:45 -0500

    writing new notes to db trying to figure out unique id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,29 @@
 const noteData = require('../db/db.json');
+const fs = require('fs');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(noteData));
 
+    const updateDb = () => {
+        fs.writeFile('db/db.json', JSON.stringify(noteData), err => {
+            if (err) throw err;
+            return true;
+        });
+    }
+
     app.post('/api/notes', (req, res) => {
-        const newNote = req.body;
-        
-        noteData.push(newNote);
-        res.json(newNote);
+        const generateNewNote = () => {
+            let newNote = req.body;
+
+            noteData.push(newNote);
+            res.json(newNote);
+            updateDb();
+        }
+
     });
+
+    app.get('/api/notes/:id', (req, res) => {
+        res.json(noteData[req.params.id]);
+    });
+
 };
