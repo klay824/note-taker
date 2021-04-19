2e660749f89c97429d9317bbd3c99a0ffb90e203
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-18 20:34:14 -0500

    added an ID generated with each new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const noteData = require('../db/db.json');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(noteData));
@@ -12,18 +13,18 @@ module.exports = (app) => {
     }
 
     app.post('/api/notes', (req, res) => {
-        const generateNewNote = () => {
-            let newNote = req.body;
+        
+        const newNote = req.body;
 
-            noteData.push(newNote);
-            res.json(newNote);
-            updateDb();
-        }
+        newNote.id = uuidv4();
+        noteData.push(newNote);
+        res.json(newNote);
+        updateDb();
 
     });
 
-    app.get('/api/notes/:id', (req, res) => {
-        res.json(noteData[req.params.id]);
-    });
+    // app.get('/api/notes/:id', (req, res) => {
+    //     res.json(noteData[req.params.id]);
+    // });
 
 };
