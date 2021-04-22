495b1174b6acd0c43011d44c2646f5c9b535b5f7
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-22 13:53:59 -0500

    removed readonly attr from handleNewNoteView

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -103,6 +103,8 @@ const handleNoteView = (e) => {
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
+    noteTitle.removeAttribute('readonly', true);
+    noteText.removeAttribute('readonly', true);
     activeNote = {};
     renderActiveNote();
 };
